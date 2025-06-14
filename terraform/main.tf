095208981d4999bbd27a562f31b70e19ec3540f3
Jofiel Arturo Salvador Contreras <jofiel21-02@outlook.es>
2025-06-13 22:22:34 -0400

    Added Terraform files for the deployment (main and variables)

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "resource_group" {
+  description = "Resource Group name"
+  type        = string
+  default     = "rg-devsu-jofiart"
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+  default     = "eastus"
+}
+
+variable "aks_cluster_name" {
+  description = "AKS Cluster name"
+  type        = string
+  default     = "aks-devsu-jofiart"
+}
