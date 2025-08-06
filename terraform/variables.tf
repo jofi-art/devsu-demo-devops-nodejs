7a7840f2b80d3ad14b1af70d4634b46e5fe482b4
Jofiel Arturo Salvador Contreras <jofiel21-02@outlook.es>
2025-08-06 10:29:39 -0400

    Testing pipeline

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group" {
   description = "Resource Group name"
   type        = string
-  default     = "rg-devsu-jofiartt"
+  default     = "rg-devsu-jofiart"
 }
 
 variable "location" {
@@ -13,5 +13,5 @@ variable "location" {
 variable "aks_cluster_name" {
   description = "AKS Cluster name"
   type        = string
-  default     = "aks-devsu-jofiartt"
+  default     = "aks-devsu-jofiart"
 }
